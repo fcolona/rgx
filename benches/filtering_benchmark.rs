e59040a9198c87ed84cf73fbf50ad82e3ef6c48d
FelipeColona <felipedev202@gmail.com>
2022-11-08 16:11:00 -0300

    feat: add benchmarking

---
+++ benches/filtering_benchmark.rs
@@ -0,0 +1,18 @@
+use rgx::filter_by_regex;
+use criterion::{
+    criterion_group,
+    criterion_main,
+    Criterion
+};
+
+fn filter_by_regex_benchmark(c: &mut Criterion){
+    let path = "/home/felipe/Desktop".to_owned();
+    let regex = r"([A-Z])\w+".to_owned();
+
+    c.bench_function("filter_by_regex", 
+        |b| b.iter(|| filter_by_regex(&path, &regex))
+    ); 
+}
+
+criterion_group!(benches, filter_by_regex_benchmark);
+criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -14,8 +14,10 @@ pub fn filter_by_regex(path: &String, regex: &String) -> Vec<String> {
         process::exit(1)
     }) {
         let entry_display = &entry.unwrap().path().display().to_string();
+        let dirs: Vec<&str> = entry_display.split("/").collect();
+        let current_sub_dir = dirs.get(dirs.len() - 1).unwrap();
 
-        let does_it_contain_filtered_text = rgx.is_match(entry_display);
+        let does_it_contain_filtered_text = rgx.is_match(current_sub_dir);
         if does_it_contain_filtered_text {
             matched_texts.push(entry_display.to_owned());
         }
