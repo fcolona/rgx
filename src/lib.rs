1850d1b9e38ab185a18ce1b0eba7da55dc3245f5
FelipeColona <felipedev202@gmail.com>
2022-11-15 21:19:04 -0300

    perf: extracted all of the unnecessary code that was running inside the loop

--- src/lib.rs
+++ src/lib.rs
@@ -18,20 +18,68 @@ pub mod ui {
         let mut stdout = io::stdout().into_raw_mode()?;
         let backend = TermionBackend::new(stdout);
         let mut terminal = Terminal::new(backend)?;
-
         let mut stdout = io::stdout().into_raw_mode()?;
 
+        let entries = filter_by_regex(path, regex);
         let new_rgx = Regex::new(&regex).unwrap();
+
+        let mut items: Vec<ListItem> = Vec::new();
+        for entry in &entries {
+            let mut spans_vec: Vec<Span> = Vec::new();
+
+            if entry.matched_text.len() != 0 {
+                let dirs: Vec<&str> = entry.path.split("/").collect();
+
+                let current_sub_dir = dirs.get(dirs.len() - 1).unwrap();
+                let precedent_dirs_array = &dirs[0..dirs.len() - 1];
+
+                let mut path_without_current_dir = String::from("");
+                for dir in precedent_dirs_array {
+                    path_without_current_dir.push_str(dir);
+                    path_without_current_dir.push_str("/");
+                }
+
+                let span_raw = Span::raw(path_without_current_dir);
+                spans_vec.push(span_raw);
+
+                for current_match in &entry.matched_text {
+                    let splits: Vec<&str> = new_rgx.split(current_sub_dir).into_iter().collect();
+
+                    let mut i = 0;
+                    while i < splits.len() - 1 {
+                        if i == 0 {
+                            let span_raw1 = Span::raw(splits.get(i).unwrap().to_owned());
+                            spans_vec.push(span_raw1);
+                        }
+
+                        let span_highlighted = Span::styled(
+                            current_match,
+                            Style::default()
+                                .fg(Color::LightYellow)
+                                .add_modifier(Modifier::BOLD),
+                        );
+
+                        let span_raw2 = Span::raw(splits.get(i + 1).unwrap().to_owned());
+
+                        spans_vec.push(span_highlighted);
+                        spans_vec.push(span_raw2);
+
+                        i = i + 1;
+                    }
+                }
+                items.push(ListItem::new(Spans::from(spans_vec)));
+            } else {
+                let span_raw = Span::raw(&entry.path);
+                items.push(ListItem::new(span_raw));
+            }
+        }
 
         let mut stdin = termion::async_stdin().keys();
         let mut s = String::new();
 
         let mut current_item_index = 0;
-
         loop {
             let input = stdin.next();
-
-            let entries = filter_by_regex(path, regex);
 
             let mut state = ListState::default();
             state.select(Some((current_item_index)));
@@ -43,82 +91,32 @@ pub mod ui {
                         if current_item_index + 1 < entries.len() - 1 {
                             current_item_index = current_item_index + 1
                         }
-                    },
+                    }
                     Key::Char('k') => {
                         if current_item_index > 0 {
                             current_item_index = current_item_index - 1
                         }
-                    },
-                    _ => {stdout.lock().flush().unwrap()}
+                    }
+                    _ => stdout.lock().flush().unwrap(),
                 }
             }
 
             terminal.draw(|rect| {
+                let items = items.clone();
+                let list = List::new(items)
+                    .style(Style::default().fg(Color::White))
+                    .highlight_style(Style::default().bg(Color::Rgb(131, 113, 163)))
+                    .highlight_symbol("> ");
+
+                let list_block = Block::default()
+                    .borders(Borders::ALL)
+                    .title(format!("{}", regex));
+
                 let size = rect.size();
                 let chunks = Layout::default()
                     .direction(Direction::Vertical)
                     .constraints([Constraint::Length(3)].as_ref())
                     .split(size);
-
-                let mut items: Vec<ListItem> = Vec::new();
-
-                for entry in &entries {
-                    let mut spans_vec: Vec<Span> = Vec::new();
-
-                    if entry.matched_text.len() != 0 {
-                        let dirs: Vec<&str> = entry.path.split("/").collect();
-                        let current_sub_dir = dirs.get(dirs.len() - 1).unwrap();
-                        let precedent_dirs_array = &dirs[0..dirs.len() - 1];
-
-                        let mut path_without_current_dir = String::from("");
-                        for dir in precedent_dirs_array {
-                            path_without_current_dir.push_str(dir);
-                            path_without_current_dir.push_str("/");
-                        }
-
-                        let span_raw = Span::raw(path_without_current_dir);
-                        spans_vec.push(span_raw);
-
-                        for current_match in &entry.matched_text {
-                            let splits: Vec<&str> =
-                                new_rgx.split(current_sub_dir).into_iter().collect();
-
-                            let mut i = 0;
-                            while i < splits.len() - 1 {
-                                if i == 0 {
-                                    let span_raw1 = Span::raw(splits.get(i).unwrap().to_owned());
-                                    spans_vec.push(span_raw1);
-                                }
-
-                                let span_highlighted = Span::styled(
-                                    current_match,
-                                    Style::default()
-                                        .fg(Color::LightYellow)
-                                        .add_modifier(Modifier::BOLD),
-                                );
-
-                                let span_raw2 = Span::raw(splits.get(i + 1).unwrap().to_owned());
-
-                                spans_vec.push(span_highlighted);
-                                spans_vec.push(span_raw2);
-
-                                i = i + 1;
-                            }
-                        }
-                        items.push(ListItem::new(Spans::from(spans_vec)));
-                    } else {
-                        let span_raw = Span::raw(&entry.path);
-                        items.push(ListItem::new(span_raw));
-                    }
-                }
-
-                let list_block = Block::default()
-                    .borders(Borders::ALL)
-                    .title(format!("{}", regex));
-                let list = List::new(items)
-                    .style(Style::default().fg(Color::White))
-                    .highlight_style(Style::default().bg(Color::Rgb(131, 113, 163)))
-                    .highlight_symbol("> ");
 
                 rect.render_stateful_widget(list.block(list_block), chunks[0], &mut state);
             });
