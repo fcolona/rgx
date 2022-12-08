657d983462fe4f707c6bad1d82f2d858801786f2
FelipeColona <felipedev202@gmail.com>
2022-12-08 00:06:40 -0300

    fix: passing a '.' as the target directory would bug the ui

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 #![feature(io_error_more)]
 #![allow(warnings, unused)]
 
-use std::{io, env, process};
+use std::{env, io, process};
 use ui::setup;
 
-pub mod ui;
 mod service;
+pub mod ui;
 
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 3 {
+    if args.len() < 1 {
         println!("ERROR: not enough arguments");
         process::exit(1)
     }
 
     let regex = &args[1];
-    let path = &args[2];
 
-    let result = setup(path, regex);    
+    let path = if args.len() == 2 || args[2].eq(".") {
+        let path_buff = env::current_dir();
+        let path = path_buff.unwrap().display().to_string();
+        path
+    } else {
+        args[2].clone()
+    };
+
+    let result = setup(&path, regex);
     return result;
 }
